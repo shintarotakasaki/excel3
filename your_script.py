c8334dd7b1c70c6d0f936e4715c00306a20f0c98
shintarotakasaki <137417713+shintarotakasaki@users.noreply.github.com>
2024-11-18 14:03:04 +0900

    your_script.py

--- your_script.py
+++ your_script.py
@@ -64,6 +64,7 @@ def main():
             # ファイルをダウンロードして一時ファイルとして保存
             response = requests.get(github_url,stream=True)
             if response.status_code == 200:
+                file_path = "伝票(規格品)_ラベル_指示書.xlsm"  # ここで file_path を定義
                 with open("伝票(規格品)_ラベル_指示書.xlsm",'wb')as f:
                     response.raw.decode_content = True
                     shutil.copyfileobj(response.raw, f)
